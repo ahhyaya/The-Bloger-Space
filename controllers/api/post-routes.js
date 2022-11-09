380eaf5697749e1e597f93a0776f23d2112f260d
doldol123 <toruig59@gmail.com>
2022-11-09 01:25:10 -0800

    complete post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,6 +3,76 @@ const { Post, Comment, User } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
+
+router.get('/', (req, res) => {
+    Post.findAll({
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => res.json(dbPostData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_detail', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this ID!' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 // GET all user posts after login
 router.post('/', withAuth, async(req, res) => {
     const body = req.body;
@@ -19,39 +89,6 @@ router.post('/', withAuth, async(req, res) => {
         console.log('Create New Post Failed!', err);
         res.status(500).json(err);
     }
-    // Post.findAll({
-    //     attributes: [
-    //         'id',
-    //         'title',
-    //         'content',
-    //         'create_at'
-    //     ],
-    //     include: [
-    //         {
-    //         model: User,
-    //         attributes: ['username']
-    //         },
-    //         {
-    //         model: Comment,
-    //         attributes: [
-    //             'id',
-    //             'comment_detail',
-    //             'post_id',
-    //             'user_id',
-    //             'create_at'
-    //         ],
-    //         include: [{
-    //             model: User,
-    //             attributes: ['username']
-    //         }]
-    //     }]
-    // })
-    //     .then(dbPostData => 
-    //         res.json(dbPostData.reverse()));
-    //      .catch (err => {
-    //     console.log(err);
-    //     res.status(500).json(err);
-    // });
 });
 
 // UPDATE post
