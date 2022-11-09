5f25f502e1006864e096a221f86ecbd1f21e0321
doldol123 <toruig59@gmail.com>
2022-11-08 21:02:57 -0800

    add click on post to dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -50,57 +50,78 @@ router.get('/', withAuth, (req, res) => {
     // });
 });
 
+// click on the post title
 router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findByPk({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'create_at'
-        ],
-        include: [
-        {
-            model: Comment,
-            attributes: [
-                'id',
-                'comment_detail',
-                'post_id',
-                'user_id',
-                'create_at'
-            ], 
-            include:
-        {
-            model: User,
-            attributes: ['username']
-        }
-    },
-    {
-        model: User,
-        attributes: ['username']
+    try {
+        const postData = await Post.findByPk(req.params.id);
+
+        if(postData) {
+            const post = postData({plain:true});
+            console.log(post)
+            res.render('edit-post', {
+                layout:'dashboard',
+                post
+            })
+        } else {
+            res.status(404).end();
+        } 
+    }catch (err) {
+            res.redirect('login');
     }
-    ]
-    })
-        .then(dbPostData => {
-            if(!dbPostData){
-                res.status(404).json({message: 'No post found!'});
-                return;
-            }
+})
 
-            const posts = dbPostData.get({ plain: true });
 
-            res.render("edit-post", {
-                posts,
-                loggedIn: true
-            });
-        });
+// router.get('/edit/:id', withAuth, (req, res) => {
+//     Post.findByPk({
+//         where: {
+//             id: req.params.id
+//         },
+//         attributes: [
+//             'id',
+//             'title',
+//             'content',
+//             'create_at'
+//         ],
+//         include: [
+//         {
+//             model: Comment,
+//             attributes: [
+//                 'id',
+//                 'comment_detail',
+//                 'post_id',
+//                 'user_id',
+//                 'create_at'
+//             ], 
+//             include:
+//         {
+//             model: User,
+//             attributes: ['username']
+//         }
+//     },
+//     {
+//         model: User,
+//         attributes: ['username']
+//     }
+//     ]
+//     })
+//         .then(dbPostData => {
+//             if(!dbPostData){
+//                 res.status(404).json({message: 'No post found!'});
+//                 return;
+//             }
+
+//             const posts = dbPostData.get({ plain: true });
+
+//             res.render("edit-post", {
+//                 posts,
+//                 loggedIn: true
+//             });
+//         });
     // .catch (err => {
     //     console.log(err);
     //     res.status(500).json(err);
     // });
-});
+// });
 
 // Create new post by logged in user
 router.get('/new', withAuth, (req, res) => {
