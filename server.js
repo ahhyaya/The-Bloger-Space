82dea384aa163844d0e746556a93a0765af95d41
doldol123 <toruig59@gmail.com>
2022-11-08 19:38:26 -0800

    fix typo in helpers util

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const handlebars = require('handlebars');
-const helper = require('./utils/helper');
-const hbs = exphbs.create( {helper} );
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create( {helpers} );
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
--- utils/helper.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    format_date: date => {
-        return `${new Date(date).getMonth() + 1}/${new DataTransfer(date).getData()}/${new Date(date).getFullYear()}`;
-    }
-}
