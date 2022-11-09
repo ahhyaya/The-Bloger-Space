72d7789c8c274be2d50f161f15a9078f9b6c61c3
doldol123 <toruig59@gmail.com>
2022-11-09 01:30:05 -0800

    fix dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -71,58 +71,6 @@ router.get('/edit/:id', withAuth, async (req, res) => {
 })
 
 
-// router.get('/edit/:id', withAuth, (req, res) => {
-//     Post.findByPk({
-//         where: {
-//             id: req.params.id
-//         },
-//         attributes: [
-//             'id',
-//             'title',
-//             'content',
-//             'create_at'
-//         ],
-//         include: [
-//         {
-//             model: Comment,
-//             attributes: [
-//                 'id',
-//                 'comment_detail',
-//                 'post_id',
-//                 'user_id',
-//                 'create_at'
-//             ], 
-//             include:
-//         {
-//             model: User,
-//             attributes: ['username']
-//         }
-//     },
-//     {
-//         model: User,
-//         attributes: ['username']
-//     }
-//     ]
-//     })
-//         .then(dbPostData => {
-//             if(!dbPostData){
-//                 res.status(404).json({message: 'No post found!'});
-//                 return;
-//             }
-
-//             const posts = dbPostData.get({ plain: true });
-
-//             res.render("edit-post", {
-//                 posts,
-//                 loggedIn: true
-//             });
-//         });
-    // .catch (err => {
-    //     console.log(err);
-    //     res.status(500).json(err);
-    // });
-// });
-
 // Create new post by logged in user
 router.get('/new', withAuth, (req, res) => {
     res.render('new-post', {
